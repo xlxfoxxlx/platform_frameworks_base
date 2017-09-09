d05134d8e280871576054df21fc4acc491c4be76
xyyx <xyyx@mail.ru>
2017-09-09 15:12:48 +0800

    base: Lockscreen Charging info  (3/3)
     based on: [2/2] frameworks: show charging current & voltage on lockscreen by
     yank555-lu [2/2]Base: show more battery info on lockscreen when charging by
     yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as
     4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    
    AICPfy:
    - default set to false
    - Current formatting improvement for devices reporting directly in Amps
    
    Return-logic of computePowerIndication has changed in Q, so adapted
    it to the new logic.
    
    Change-Id: Icc08f6f7b0fd31cb5ab31556c6a18ee9bc57bf22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4504,6 +4504,12 @@ public final class Settings {
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
         /**
+        * Whether to show the battery info on the lockscreen while charging
+        * @hide
+        */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
