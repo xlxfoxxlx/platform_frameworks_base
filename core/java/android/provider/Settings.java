dfaa741dcae8431a5771191a3225b59413ed9d5f
Nico60 <ndhouailly@gmail.com>
2019-03-14 07:04:00 -0400

    TrafficSB: Hide arrows in statusbar traffic indicators [1/2]
    arrow-edits:
    adapt code to our dynamic arrows method
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5545,6 +5545,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
