i-o-n/frameworks_base

Fix show IME space option
fix following show IME space option issues -

issue 1: switch to gesture navigation and disable IME space
	outcome: gesturebar handle gone missing

issue 2: go through steps in issue 1 and now switch to button mode navigation
	outcome: navigation buttons gone missing

Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -35,6 +35,7 @@ import android.widget.LinearLayout;
 import android.widget.Space;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.ion.IonUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.CustomSettingsService;
@@ -165,12 +166,14 @@ public class NavigationBarInflaterView extends FrameLayout
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_INVERSE);
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
         Dependency.get(CustomSettingsService.class).addIntObserver(this, Settings.System.NAVIGATION_BAR_ARROW_KEYS);
+        Dependency.get(CustomSettingsService.class).addIntObserver(this, Settings.System.NAVIGATION_BAR_IME_SPACE);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         Dependency.get(NavigationModeController.class).removeListener(this);
         Dependency.get(TunerService.class).removeTunable(this);
+        Dependency.get(CustomSettingsService.class).removeObserver(this);
         Dependency.get(CustomSettingsService.class).removeObserver(this);
         super.onDetachedFromWindow();
     }
@@ -536,8 +539,12 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     private boolean showDpadArrowKeys() {
-        return Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.NAVIGATION_BAR_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0;
+        return ((Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0)
+                && ((Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_IME_SPACE, 1, UserHandle.USER_CURRENT) != 0)
+                || (IonUtils.isThemeEnabled("com.android.internal.systemui.navbar.twobutton")
+                || IonUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton"))));
     }
 
     @Override

xlxfoxxlx/platform_frameworks_base

Change-Id: I08a416e213b5a322fb58bd8a407b78cf736ed298

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -35,6 +35,7 @@ import android.widget.LinearLayout;
 import android.widget.Space;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.aosip.aosipUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.aosip.AOSiPSettingsService;
@@ -165,6 +166,7 @@ public class NavigationBarInflaterView extends FrameLayout
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_INVERSE);
         Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
         Dependency.get(AOSiPSettingsService.class).addIntObserver(this, Settings.System.NAVIGATION_BAR_ARROW_KEYS);
+        Dependency.get(AOSiPSettingsService.class).addIntObserver(this, Settings.System.NAVIGATION_BAR_IME_SPACE);
     }
 
     @Override
@@ -536,8 +538,12 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     private boolean showDpadArrowKeys() {
-        return Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.NAVIGATION_BAR_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0;
+        return ((Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0)
+                && ((Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_IME_SPACE, 1, UserHandle.USER_CURRENT) != 0)
+                || (aosipUtils.isThemeEnabled("com.android.internal.systemui.navbar.twobutton")
+                || aosipUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton"))));
     }
 
     @Override